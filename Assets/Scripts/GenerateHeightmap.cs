a37df795b5473d88a5647332f337671c7ef2cc97
Alexander Zacherl <alexander.zacherl@googlemail.com>
2019-05-29 12:59:15 +0100

    generating weird tiny worlds

--- Assets/Scripts/GenerateHeightmap.cs
+++ Assets/Scripts/GenerateHeightmap.cs
@@ -21,13 +21,15 @@ public class GenerateHeightmap : MonoBehaviour {
 
     // The number of cycles of the basic noise pattern that are repeated
     // over the width and height of the texture.
-    public float scale = 1.0F;
+    float scale = 1.0F;
 
     private Texture2D noiseTex;
     private Color[] pix;
     private Renderer rend;
 
     void Start () {
+        scale = Random.Range(1f, 3f);
+
         rend = GetComponent<Renderer>();
 
         // Set up the texture and a Color array to hold pixels during processing.
@@ -39,10 +41,9 @@ public class GenerateHeightmap : MonoBehaviour {
         byte[] bytes = noiseTex.EncodeToPNG();
         File.WriteAllBytes(Application.dataPath + "/../heightmap.png", bytes);
 
-        RenderTexture rt = new RenderTexture(512, 512, 16, RenderTextureFormat.ARGB32);
+        RenderTexture rt = new RenderTexture(512, 512, 16, RenderTextureFormat.Default);
         rt.Create();
         Graphics.Blit(noiseTex, rt);
-
         UpdateTerrainData(rt);
     }
 
@@ -56,17 +57,26 @@ public class GenerateHeightmap : MonoBehaviour {
         // For each pixel in the texture...
         float y = 0.0F;
 
+        //float minV = 0.5f;
+        //float maxV = 0.5f;
+
         while (y < noiseTex.height) {
             float x = 0.0F;
+
             while (x < noiseTex.width) {
                 float xCoord = xOrg + x / noiseTex.width * scale;
                 float yCoord = yOrg + y / noiseTex.height * scale;
-                float sample = Mathf.PerlinNoise(xCoord, yCoord);
+                float sample = Mathf.PerlinNoise(xCoord, yCoord) + 0.25f;
+                //if (sample < minV) minV = sample;
+                //if (sample > maxV) maxV = sample;
                 pix[(int)y * noiseTex.width + (int)x] = new Color(sample, sample, sample);
                 x++;
             }
             y++;
         }
+
+        //print(minV);
+        //print(maxV);
 
         // Copy the pixel data to the texture and load it into the GPU.
         noiseTex.SetPixels(pix);
