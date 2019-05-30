31c4e4b60665e7e515ec1f1f915a734a87994150
Alexander Zacherl <alexander.zacherl@googlemail.com>
2019-05-30 16:13:10 +0100

    new noise gen, still static

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 
 // Create a texture and fill it with Perlin noise.
@@ -28,18 +29,51 @@ public class MapGenerator : MonoBehaviour {
     private Color[] pix;
     private Renderer rend;
 
+    private void Start () {
+        RT = new RenderTexture(Data.heightMapWidth, Data.heightMapHeight, 16, RenderTextureFormat.ARGB1555);
+        RT.Create();
+    }
+
     void Update () {
         if (Input.GetKeyDown(KeyCode.G)) {
             print("generating a map");
-            GenerateMap();
+            //GenerateMap();
+            Texture2D heightMap = GenerateHeightMap(Data.heightMapWidth, Data.heightMapHeight);
+            WriteHeightMapToFile(heightMap);
+            LoadHeightmapFromFile(Data.pathHeightMap);
         }
 
         if (Input.GetKeyDown(KeyCode.L)) {
             print("loading a map");
             SaveToImage.SaveImage(Steganography.RecoverImage(Data.pathCombined, Data.screenShotWidth, Data.screenShotHeight, Data.bitsHidden, Data.heightMapWidth, Data.heightMapHeight), Data.pathRecovered);
-            LoadHeightmapFromScreenshot(Data.pathRecovered);
+            LoadHeightmapFromFile(Data.pathRecovered);
         }
     }
+
+    Texture2D GenerateHeightMap (int width, int height) {
+        Texture2D heightmap = new Texture2D(width, height);
+        List<Color32> list = new List<Color32>();
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                float nx = (float)x / (float)width - 0.5f;
+                float ny = (float)y / (float)height - 0.5f;
+                float v = Mathf.PerlinNoise(nx, ny);
+                Color32 c = new Color32((byte)(v*255), (byte)(v * 255), (byte)(v * 255), 255);
+                list.Add(c);
+            }
+        }
+        Color32[] colors = list.ToArray();
+        heightmap.SetPixels32(colors);
+        return heightmap;
+    }
+
+    void WriteHeightMapToFile(Texture2D tex) {
+        byte[] bytes = tex.EncodeToPNG();
+        File.WriteAllBytes(Application.dataPath + Data.pathHeightMap, bytes);
+    }
+
+
 
     void GenerateMap () {
         scale = Random.Range(1f, 3f);
@@ -89,11 +123,12 @@ public class MapGenerator : MonoBehaviour {
         noiseTex.Apply();
     }
 
-    void LoadHeightmapFromScreenshot(string path) {
+    void LoadHeightmapFromFile(string path) {
         byte[] bytes = File.ReadAllBytes(Application.dataPath + path);
         Texture2D tex = new Texture2D(Data.heightMapWidth, Data.heightMapHeight);
         tex.LoadImage(bytes);
         Graphics.Blit(tex, RT);
         UpdateTerrainData(RT);
     }
+
 }
