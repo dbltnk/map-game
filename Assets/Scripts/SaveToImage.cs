a91179266f276a38870aef86c40905703b1f9ce8
Alexander Zacherl <alexander.zacherl@googlemail.com>
2019-05-30 11:21:07 +0100

    steganography works, lots of loss though

--- Assets/Scripts/SaveToImage.cs
+++ Assets/Scripts/SaveToImage.cs
@@ -26,81 +26,9 @@ public class SaveToImage : MonoBehaviour
             print("combining images");
 
             //HideImageAinB(pixA, pixB);
-            SaveImage(HideImage("/../map.png", "/../heightmap.png", 4), "combined");
+            SaveImage(Steganography.HideImage("/../map.png", "/../heightmap.png", 4), "combined");
+            SaveImage(Steganography.RecoverImage("/../combined.png", 4, 512, 512), "recovered");
         }
-    }
-
-    // Hide bm_hidden inside bm_visible and return the result.
-    // Source: http://csharphelper.com/blog/2016/09/use-steganography-to-hide-one-picture-inside-another-in-c/
-    Texture2D HideImage (string path_visible, string path_hidden, int hidden_bits) 
-    {
-        byte[] bytesVisible = File.ReadAllBytes(Application.dataPath + path_visible);
-        Texture2D texVisible = new Texture2D(1024, 1024);
-        texVisible.LoadImage(bytesVisible);
-        Color32[] pixVisible = texVisible.GetPixels32();
-
-        byte[] bytesHidden = File.ReadAllBytes(Application.dataPath + path_hidden);
-        Texture2D texHidden = new Texture2D(512, 512);
-        texHidden.LoadImage(bytesHidden);
-        Color32[] pixHidden = texHidden.GetPixels32();
-               
-        int shift = (8 - hidden_bits);
-        int visible_mask = 0xFF << hidden_bits;
-        int hidden_mask = 0xFF >> shift;
-        Color32[] pixCombined = new Color32[pixVisible.Length];
-
-        for (int x = 0; x < pixVisible.Length; x++) {
-
-            Color32 clr_visible = pixVisible[x];
-
-            // the hidden image is generally smaller than the visible one so we keep the visible pics color data intact
-            if (x >= pixHidden.Length) {
-                pixCombined[x] = new Color32(clr_visible.r, clr_visible.g, clr_visible.b, clr_visible.a);
-            }
-            else {
-                Color32 clr_hidden = pixHidden[x];
-
-                int r = (clr_visible.r & visible_mask) +
-                    ((clr_hidden.r >> shift) & hidden_mask);
-                int g = (clr_visible.g & visible_mask) +
-                    ((clr_hidden.g >> shift) & hidden_mask);
-                int b = (clr_visible.b & visible_mask) +
-                    ((clr_hidden.b >> shift) & hidden_mask);
-                int a = (clr_visible.a & visible_mask) +
-                    ((clr_hidden.a >> shift) & hidden_mask);
-
-                pixCombined[x] = new Color32((byte)r, (byte)g, (byte)b, (byte)a);
-            }
-        }
-
-        Texture2D texCombined = new Texture2D(texVisible.width, texVisible.height);
-        texCombined.SetPixels32(pixCombined);
-
-        return texCombined;
-    }
-
-    Texture2D HideImageAinB (Color32[] A, Color32[] B) {
-
-        print("-----------AAAAAAA-----------------");
-        int cA = 0;
-        foreach (Color pixel in A) {
-            if (cA >= 10) break;
-            cA++;
-            print(string.Concat(pixel.r, " ", pixel.g, " ", pixel.b, " ", pixel.a, " "));
-            print(pixel);
-        }
-        print("-----------------------------------");
-        print("-----------BBBBBBB-----------------");
-        int cB = 0;
-        foreach (Color pixel in B) {
-            if (cB >= 10) break;
-            cB++;
-            print(string.Concat(pixel.r, " ", pixel.g, " ", pixel.b, " ", pixel.a, " "));
-            print(pixel);
-        }
-        print("-----------------------------------");
-        Texture2D C = new Texture2D(512, 512);
-        return C;
     }
 
     // Take a "screenshot" of a camera's Render Texture.
---
+++ Assets/Scripts/Steganography.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Steganography : MonoBehaviour
+{
+    // Hide bm_hidden inside bm_visible and return the result.
+    // Source: http://csharphelper.com/blog/2016/09/use-steganography-to-hide-one-picture-inside-another-in-c/
+    public static Texture2D HideImage (string path_visible, string path_hidden, int hidden_bits) {
+        byte[] bytesVisible = File.ReadAllBytes(Application.dataPath + path_visible);
+        Texture2D texVisible = new Texture2D(1024, 1024);
+        texVisible.LoadImage(bytesVisible);
+        Color32[] pixVisible = texVisible.GetPixels32();
+
+        byte[] bytesHidden = File.ReadAllBytes(Application.dataPath + path_hidden);
+        Texture2D texHidden = new Texture2D(512, 512);
+        texHidden.LoadImage(bytesHidden);
+        Color32[] pixHidden = texHidden.GetPixels32();
+
+        int shift = (8 - hidden_bits);
+        int visible_mask = 0xFF << hidden_bits;
+        int hidden_mask = 0xFF >> shift;
+        Color32[] pixCombined = new Color32[pixVisible.Length];
+
+        for (int x = 0; x < pixVisible.Length; x++) {
+
+            Color32 clr_visible = pixVisible[x];
+
+            // the hidden image is generally smaller than the visible one so we keep the visible pics color data intact
+            if (x >= pixHidden.Length) {
+                pixCombined[x] = new Color32(clr_visible.r, clr_visible.g, clr_visible.b, clr_visible.a);
+            } else {
+                Color32 clr_hidden = pixHidden[x];
+
+                int r = (clr_visible.r & visible_mask) +
+                    ((clr_hidden.r >> shift) & hidden_mask);
+                int g = (clr_visible.g & visible_mask) +
+                    ((clr_hidden.g >> shift) & hidden_mask);
+                int b = (clr_visible.b & visible_mask) +
+                    ((clr_hidden.b >> shift) & hidden_mask);
+                int a = (clr_visible.a & visible_mask) +
+                    ((clr_hidden.a >> shift) & hidden_mask);
+
+                pixCombined[x] = new Color32((byte)r, (byte)g, (byte)b, (byte)a);
+            }
+        }
+
+        Texture2D texCombined = new Texture2D(texVisible.width, texVisible.height);
+        texCombined.SetPixels32(pixCombined);
+
+        return texCombined;
+    }
+
+    // Recover a hidden image.
+    // Source: http://csharphelper.com/blog/2016/09/use-steganography-to-hide-one-picture-inside-another-in-c/
+    public static Texture2D RecoverImage (string path_combined, int hidden_bits, int picHiddenWidth, int picHiddenHeight) {
+        byte[] bytesCombined = File.ReadAllBytes(Application.dataPath + path_combined);
+        Texture2D texCombined = new Texture2D(1024, 1024);
+        texCombined.LoadImage(bytesCombined);
+        Color32[] pixCombined = texCombined.GetPixels32();
+
+        int shift = (8 - hidden_bits);
+        int hidden_mask = 0xFF >> shift;
+        Color32[] pixRecovered = new Color32[picHiddenWidth * picHiddenHeight];
+
+        for (int x = 0; x < pixCombined.Length; x++) {
+
+            if (x >= pixRecovered.Length) {
+                break;
+            } else {
+                Color32 clr_combined = pixCombined[x];
+
+                int r = (clr_combined.r & hidden_mask) << shift;
+                int g = (clr_combined.g & hidden_mask) << shift;
+                int b = (clr_combined.b & hidden_mask) << shift;
+                int a = (clr_combined.a & hidden_mask) << shift;
+
+                pixRecovered[x] = new Color32((byte)r, (byte)g, (byte)b, (byte)a);
+            }
+        }
+        Texture2D texRecovered = new Texture2D(picHiddenWidth, picHiddenHeight);
+        texRecovered.SetPixels32(pixRecovered);
+
+        return texRecovered;
+    }
+}
