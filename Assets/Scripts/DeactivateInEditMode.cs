1455716e88a8091d116532209ab87abdb9f70cf9
Alexander Zacherl <alexander.zacherl@googlemail.com>
2019-05-31 12:04:03 +0100

    memory leak fix hack

---
+++ Assets/Scripts/DeactivateInEditMode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// THIS IS AN UGLY HACK TO PREVENT THE TERRAIN MEMORY LEAK IN THE SCENE VIEW FROM HAPPENING
+
+[ExecuteInEditMode]
+public class DeactivateInEditMode : MonoBehaviour
+{
+    Terrain t;
+
+    private void Start () {
+        t = GetComponent<Terrain>();
+    }
+
+    void Update()
+    {
+        if (Application.IsPlaying(gameObject)) {
+            t.enabled = true;
+        }
+        else {
+            t.enabled = false;
+        }
+    }
+}
