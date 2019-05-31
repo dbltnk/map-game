f314fd852a16dfadf4806c265b13ade23de91899
Alexander Zacherl <alexander.zacherl@googlemail.com>
2019-05-31 13:37:00 +0100

    minimap works

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -11,10 +11,20 @@ public class Data : MonoBehaviour
     public static int ScreenShotWidth = 1024;
     public static int ScreenShotHeight = 1024;
 
+    public static float TerrainWidth = 1000;
+    public static float TerrainHeight = 1000;
+
+    public static float MiniMapWidth = 250;
+    public static float MiniMapHeight = 250;
+
     public static string PathHeightMap = "/../1_heightmap.png";
     public static string PathScreenShot = "/../2_screenshot.png";
     public static string PathCombined = "/../3_combined.png";
     public static string PathRecovered = "/../4_recovered.png";
 
     public static int BitsHidden = 5;
+
+    public static float MapIntoRange (float value, float from1, float to1, float from2, float to2) {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
 }
---
+++ Assets/Scripts/PlayerPosition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPosition : MonoBehaviour
+{
+    public GameObject Player;
+
+    void Update()
+    {
+        float pX = Data.MapIntoRange(Player.transform.position.x, 0, Data.TerrainWidth, -1f * Data.MiniMapWidth / 2, Data.MiniMapWidth / 2);
+        float pZ = Data.MapIntoRange(Player.transform.position.z, 0, Data.TerrainHeight, -1f * Data.MiniMapWidth / 2, Data.MiniMapWidth / 2);
+        gameObject.transform.localPosition = new Vector3(pX, pZ, 0f);
+    }
+}
