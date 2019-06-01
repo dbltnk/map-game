582b4439a23653613207ad4fec98fb96bc3bf34a
Alexander Zacherl <alexander.zacherl@googlemail.com>
2019-06-01 16:19:43 +0100

    fix yield

--- Assets/Scripts/SetVisitedMap.cs
+++ Assets/Scripts/SetVisitedMap.cs
@@ -11,12 +11,12 @@ public class SetVisitedMap : MonoBehaviour
     Sprite overrideSprite;
 
     // Start is called before the first frame update
-    void Start()
+    IEnumerator Start()
     {
         img = GetComponent<Image>();
         pP = mapMarker.GetComponent<PlayerPosition>();
+        while (pP.VisitedTex == null) yield return null;
         overrideSprite = Sprite.Create(pP.VisitedTex, new Rect(0.0f, 0.0f, pP.VisitedTex.width, pP.VisitedTex.height), new Vector2(0.5f, 0.5f), 1.0f);
-
     }
 
     // Update is called once per frame
